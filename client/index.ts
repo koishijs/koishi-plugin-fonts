87dd2a87353188f1f7b114b8fe9cc9077bf6aa8b
Maiko Tan <maiko.tan.coding@gmail.com>
2023-11-24 10:22:54 +0800

    fix: impossible d'assigner fonts au fields

--- client/index.ts
+++ client/index.ts
@@ -1,10 +1,12 @@
 import { Context } from '@koishijs/client'
 import Layout from './layout.vue'
+import type {} from '..'
 
 export default (ctx: Context) => {
   ctx.page({
     name: '字体管理',
     path: '/fonts',
     component: Layout,
+    fields: ['fonts'],
   })
 }
