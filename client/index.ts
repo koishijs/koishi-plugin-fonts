0cf16b2187621e1358f294f94c784e9108cc08c1
霞飛 <saarchaffee@qq.com>
2025-05-06 01:16:00 +0800

    feat: add icon

--- client/index.ts
+++ client/index.ts
@@ -1,11 +1,14 @@
-import { Context } from '@koishijs/client'
+import { Context, icons } from '@koishijs/client'
 
+import icom from './icon.vue'
 import layout from './layout.vue'
+icons.register('fonts', icom)
 
 export default (ctx: Context) => {
   ctx.page({
     name: '字体管理',
     path: '/fonts',
+    icon: 'fonts',
     component: layout,
     fields: ['fonts'],
   })
